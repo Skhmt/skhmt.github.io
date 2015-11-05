37eb894fd108a4aff607ca36940299c85403b7b6
Skhmt <michael.shinn@outlook.com>
2015-11-04 17:21:33 -0800

    oops

--- koala/scripts.js
+++ koala/scripts.js
@@ -25,7 +25,7 @@ function kraken2(userdata) {
 
 // Setting up the main page area
 document.getElementById("twitchChat").data="http://www.twitch.tv/" + username + "/chat";
-document.getElementById("twitchChat").style.display = block;
+document.getElementById("twitchChat").style.display = "block";
 //horizontalSizeElements();
 loadScript();
 
