3eeed39a9efdc49d6763dba901c01822a0b538dc
Skhmt <michael.shinn@outlook.com>
2015-11-30 14:36:17 -0800

    Update scripts.js

--- bot/scripts.js
+++ bot/scripts.js
@@ -8,27 +8,66 @@ var access_token = "";
 var irc = "";
 var bot = "";
 var server = "irc.twitch.tv";
+var fs;
+var logFile;
+var execPath;
+var hosts = [];
+var hostFile;
 
 $(document).ready(function(){
 	
-	var fs = require("fs");
+	// Setting up jQuery elements
+	$("#tabs").tabs();
+
+	// Setting up file read stuff and variables
+	fs = require("fs");
 	var path = require("path");
 	
-	var execPath = path.dirname(process.execPath);
-	execPath += "\\info.ini";
-	var readFile = fs.readFileSync(execPath);
-	var data = JSON.parse( readFile );
+	execPath = path.dirname(process.execPath);
+	var readFile = fs.readFileSync(execPath + "\\info.ini");
+	var data = $.parseJSON( readFile );
 	
 	access_token = data.oauth;
 	username = data.username;
 	channel = "#" + username;
 	
-	log("* Username: "+username);
-	
-	runBot();
+	// Set up the chat log
+	var d = new Date();
+	var dmonth = d.getMonth() < 10 ? "0" + d.getMonth() : d.getMonth();
+	var dday = d.getDate() < 10 ? "0" + d.getDate() : d.getDate();
+	var dhour = d.getHours() < 10 ? "0" + d.getHours() : d.getHours();
+	var dmin = d.getMinutes() < 10 ? "0" + d.getMinutes() : d.getMinutes();
+	var dsec = d.getSeconds() < 10 ? "0" + d.getSeconds() : d.getSeconds();
+	var logname = "chatlog_" + d.getFullYear() + "-" + dmonth + "-" + dday + "_" + dhour + "-" + dmin + "-" + dsec + ".log";
+	logFile = execPath + "\\" + logname;
+
+	/*
+	var firstLogLine =  d.getFullYear() + "-" + dmonth + "-" + dday + " " + dhour + ":" + dmin + ":" + dsec + "\r\n";
+	firstLogLine += "-------------------\r\n";
+	fs.appendFile(logFile, firstLogLine, function (err) {
+		if (err) {
+			$("#console").append("* Error writing to log" + "<br />");
+		}
+	});
+	*/
+
+	// Set up host log
+	hostFile = execPath + "\\hosts.log";
+	fs.writeFile(hostFile, "", function (err) {
+		if (err) {
+			log("* Error clearing the host file");
+		}
+	});
+
+	// Running tabs
+	runChat();
+
+	var refresh = 10;
+	setInterval(getHosts(), refresh*1000);
+
 });
 
-function runBot() {
+function runChat() {
 	
 	irc = require("irc");
 	
@@ -66,16 +105,12 @@ function runBot() {
 	});
 	
 	bot.addListener("message", function(from, to, text, message){
-		log(getTS() + " " + from + ":&nbsp;&nbsp;" + text);
+		log(getTimeStamp() + " " + from + ": " + text);
 	});
 	
 	bot.addListener("action", function(from, to, text, message){
-		log(getTS() + " " + from + " " + text);
+		log(getTimeStamp() + " " + from + " " + text);
 	});
-}
-
-function say(message) {
-	bot.say(channel, message);
 }
 
 function log(message) {
@@ -87,17 +122,24 @@ function log(message) {
 	
 	if(isScrolledToBottom)
 		out.scrollTop = out.scrollHeight - out.clientHeight;
+
+	// write to log
+	fs.appendFile(logFile, message + "\r\n", function (err) {
+		if (err) {
+			$("#console").append("* Error writing to log" + "<br />");
+		}
+	});
 }
 
 function chat() {
 	var text = $("#chatText").val();
-	say(text);
-	log("* " + getTS() + " " + username + ":&nbsp;&nbsp;" + text);
+	bot.say(channel, text);
+	log(getTimeStamp() + " " + username + ": " + text);
 	
 	$("#chatText").val("");
 }
 
-function getTS() {
+function getTimeStamp() {
 	var dt = new Date();
 	var hrs = dt.getHours();
 	var mins = dt.getMinutes();
@@ -108,4 +150,54 @@ function getTS() {
 	if (secs < 10) secs = "0" + secs;
 	
 	return "[" + hrs + ":" + mins + "]";
+}
+
+function getHosts() {
+	// get id
+	var id;
+	$.getJSON(
+		"https://api.twitch.tv/kraken/channels/"+username,
+		{
+			"client_id" : clientid,
+			"api_version" : 3
+		},
+		function(response){
+			id = response._id;
+
+			// get hosts into json
+			var newHosts = [];
+			$.getJSON(
+				"http://tmi.twitch.tv/hosts",
+				{
+					"include_logins" : "1",
+					"target" : id
+				},
+				function(response){
+					for (var i = 0; i < response.hosts.length; i++){
+						newHosts.push(response.hosts[i].host_login);
+					}
+
+					updateHosts(newHosts);
+				}
+			);
+		}
+	);
+}
+
+function updateHosts(newHosts){
+	for (var i = 0; i < newHosts.length; i++){
+		if (hosts.indexOf(newHosts[i]) == -1) { // if the host is not in the current list of hosts 
+			var theHost = newHosts[i];
+			hosts.push(theHost); // add to the list of hosts to prevent duplicates
+			$("#hosts").append(getTimeStamp() + " " + theHost + "<br />"); // adding to the hosts tab
+			log("* " + getTimeStamp() + " " + theHost + " is hosting you"); // logging it
+
+			// write to host file
+			fs.appendFile(hostFile, theHost + "\r\n", function (err) {
+				if (err) {
+					log("* Error writing to host file");
+				}
+			});
+		}
+	}
 }
