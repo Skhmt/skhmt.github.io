4b541799b71c821491b186a194c4be9bc4d579d3
Skhmt <Skhmt@users.noreply.github.com>
2016-04-06 08:28:54 -0700

    Update scripts.js

--- koala/scripts.js
+++ koala/scripts.js
@@ -20,7 +20,7 @@ $(document).ready(function(){
 	access_token = document.location.hash.substring(14,44);
 
 	/* masking url, specifically the access_token */
-	history.pushState({}, "masked url", "/");
+	history.pushState({}, "masked url", "/koala/");
 
 	/* Detecting browser... if mobile, remove the videos and resize the remaining elements. */
 	if (
