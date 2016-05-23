604010114420cc68a81101292764140704f2bc4e
Skhmt <Skhmt@users.noreply.github.com>
2016-05-22 19:52:14 -0700

    Update app.js

--- autohost/app.js
+++ autohost/app.js
@@ -1,26 +1,35 @@
+
+// https://www.reddit.com/r/Twitch/comments/4kd39i/looking_for_autohost_bot_for_linux/
+
+
 var clientid = 'mmkfyou3e4jgui4y57xxhag67apzdv9'; // public client id
 var oauth = ''; // oauth without "oauth:", requires chat_login
 var channels = [];
-var index = 0;
 
 $(function(){
-  /* If there's no auth, send the user to the login page */
+
+
+  login(clientid);
+});
+
+function login(client) {
+  // If there's no auth, send the user to the login page
   if ( document.location.hash.length < 50 ) {
-    window.location = 'https://api.twitch.tv/kraken/oauth2/authorize?response_type=token&' +
-    'client_id=' + clientid +
-    '&redirect_uri=https://skhmt.github.io/autohost/?scope=chat_login&force_verify=true';
+    window.location = `https://api.twitch.tv/kraken/oauth2/authorize?response_type=token
+    &client_id=${client}
+    &redirect_uri=https://skhmt.github.io/autohost/?scope=chat_login&force_verify=true`;
   }
   else {
     oauth = document.location.hash.substring(14,44);
-    history.pushState({}, "", "/getmods/"); // masking the url
-    TAPIC.setup(clientid, oauth, function(username) {
+    history.pushState({}, '', '/autohost/'); // masking the url
+    TAPIC.setup(client, oauth, function(username) {
       getFollowedChannels( username, 0 );
     } );
   }
-});
+}
 
 function getFollowedChannels(username, offset) {
-  $.getJSON('https://api.twitch.tv/kraken/users/' + username + '/follows/channels?callback=?',
+  $.getJSON(`https://api.twitch.tv/kraken/users/${username}/follows/channels?callback=?`,
     { "limit":100, "offset": offset, "sortby": 'last_broadcast' },
     function(res) {
       var folLen = res.follows.length;
@@ -38,7 +47,7 @@ function getFollowedChannels(username, offset) {
         return getFollowedChannels(username, (offset+100) );
       }
       else {
-        outputChannels(username, offset+folLen);
+        // outputChannels(username, offset+folLen);
       }
     } // anonymous function
   ); // getJSON
