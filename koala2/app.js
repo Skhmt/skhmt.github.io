aae7081d991e098e6cc58bedc9ae4dd9df73c6ac
Skhmt <Skhmt@users.noreply.github.com>
2016-07-08 20:08:23 -0700

    Update app.js

--- koala2/app.js
+++ koala2/app.js
@@ -1,143 +1,152 @@
 var username = '';
 
-$( function() {
-	var clientid = '3s27p59atb0bwu6vkh2rnrxeqx3dn3h'; //streamkoala2 public client id
-	var oauth = document.location.hash.substring(14,44);
-	
-	history.pushState({}, "", "/koala2/"); // masking the url
+$(function() {
+    var clientid = '3s27p59atb0bwu6vkh2rnrxeqx3dn3h'; //streamkoala2 public client id
+    var oauth = document.location.hash.substring(14, 44);
 
-	TAPIC.setup( clientid, oauth, function(user) {
-		username = user;
-		$('#username').html( username );
-		var channel = username;
-		TAPIC.runChat( channel, function() {
-			$('#twitchVideo').attr( 'src', 'http://player.twitch.tv/?autoplay=true&muted=true&channel=' + channel );
-			$('#chatframe').attr( 'src', 'http://www.twitch.tv/' + channel + '/chat' );
-			$('#username').html( TAPIC.getDisplayName() );
-			refresh(5);
-		} );
-	} );
+    history.pushState({}, "", "/koala2/"); // masking the url
 
-	// UI
+    TAPIC.setup(clientid, oauth, function(user) {
+        username = user;
+        $('#username').html(username);
+        var channel = username;
+        TAPIC.runChat(channel, function() {
+            $('#twitchVideo').attr('src', 'http://player.twitch.tv/?autoplay=true&muted=true&channel=' + channel);
+            $('#chatframe').attr('src', 'http://www.twitch.tv/' + channel + '/chat');
+            $('#username').html(TAPIC.getDisplayName());
+            refresh(5);
+        });
+    });
 
-	$('.updateStatusButton').click( function() {
-		changeStatusOpen();
-	} );
+    // UI
 
-	$('#saveStatusButton').click( function() {
-		changeStatusSubmit();
-	} );
-	
-	$("#sortable").sortable({
-		handle: ".handle"
-	});
-} );
+    $('#updateStatusButton').click(function() {
+        changeStatusSubmit();
+    });
 
-function refresh( seconds ) {
-	// online status
-	if ( TAPIC.isOnline() ) {
-		document.getElementById('username').className = 'text-success';
-	}
-	else {
-		document.getElementById('username').className = 'text-danger';
-	}
+    $('#updateGameButton').click(function() {
+        changeStatusSubmit();
+    });
 
-	$('#game').html( TAPIC.getGame().replace(/</g, '&lt;').replace(/\(/g, '&#40;') );
-	$('#status').html( TAPIC.getStatus().replace(/</g, '&lt;').replace(/\(/g, '&#40;') );
+    $("#sortable").sortable({
+        handle: ".handle"
+    });
+});
 
-	updateUserlist();
-	updateViewers();
+function refresh(seconds) {
+    // online status
+    if (TAPIC.isOnline()) {
+        document.getElementById('username').className = 'text-success';
+    } else {
+        document.getElementById('username').className = 'text-danger';
+    }
 
-	if ( seconds ) {
-		setTimeout( function() {
-			refresh(seconds);
-		}, seconds*1000 );
-	}
+    $('#game').html(TAPIC.getGame().replace(/</g, '&lt;').replace(/\(/g, '&#40;'));
+    $('#status').html(TAPIC.getStatus().replace(/</g, '&lt;').replace(/\(/g, '&#40;'));
+
+    updateUserlist();
+    updateViewers();
+
+    if (seconds) {
+        setTimeout(function() {
+            refresh(seconds);
+        }, seconds * 1000);
+    }
 }
 
 function updateViewers() {
-	$('#viewercount').html( `
+    $('#viewercount').html(`
 		<i class="fa fa-user fa-fw text-info"></i>
 		&nbsp;&nbsp;&nbsp;${TAPIC.getCurrentViewCount().toLocaleString()}
 		<br>
 		<i class="fa fa-heart fa-fw text-danger"></i>
-		&nbsp;&nbsp;&nbsp;${TAPIC.getFollowerCount().toLocaleString()}`
-	);
+		&nbsp;&nbsp;&nbsp;${TAPIC.getFollowerCount().toLocaleString()}`);
 }
 
 function updateUserlist() {
-	var output = '';
-	if ( !TAPIC.getChatters().staff ) return;
+    var output = '';
+    if (!TAPIC.getChatters().staff) return;
 
-	if ( TAPIC.getChatters().staff.length > 0  ) {
-		output += `<p> <b style="color: #C9F;">STAFF</b> &mdash;
+    if (TAPIC.getChatters().staff.length > 0) {
+        output += `<p> <b style="color: #C9F;">STAFF</b> &mdash;
 			<b>${TAPIC.getChatters().staff.length.toLocaleString()}</b> <br> `;
 
-		for (var i = 0; i < TAPIC.getChatters().staff.length; i++) {
-			var tempuser = TAPIC.getChatters().staff[i];
-			output += `${tempuser} <br> `;
-		}
-		output += '</p> ';
-	}
+        for (var i = 0; i < TAPIC.getChatters().staff.length; i++) {
+            var tempuser = TAPIC.getChatters().staff[i];
+            output += `${tempuser} <br> `;
+        }
+        output += '</p> ';
+    }
 
-	if ( TAPIC.getChatters().moderators.length > 0 ) {
-		output += `<p> <b style="color: #34ae0a;">MODS</b> &mdash;
+    if (TAPIC.getChatters().moderators.length > 0) {
+        output += `<p> <b style="color: #34ae0a;">MODS</b> &mdash;
 			<b>${TAPIC.getChatters().moderators.length.toLocaleString()}</b> <br> `;
 
-		for (var i = 0; i < TAPIC.getChatters().moderators.length; i++) {
-			var tempuser = TAPIC.getChatters().moderators[i];
-			output += `${tempuser} <br> `;
-		}
-		output += '</p> ';
-	}
+        for (var i = 0; i < TAPIC.getChatters().moderators.length; i++) {
+            var tempuser = TAPIC.getChatters().moderators[i];
+            output += `${tempuser} <br> `;
+        }
+        output += '</p> ';
+    }
 
-	if ( TAPIC.getChatters().admins.length > 0 ) {
-		output += `<p> <b style="color: #faaf19;">ADMINS</b> &mdash;
+    if (TAPIC.getChatters().admins.length > 0) {
+        output += `<p> <b style="color: #faaf19;">ADMINS</b> &mdash;
 			<b>${TAPIC.getChatters().admins.length.toLocaleString()}</b> <br> `;
 
-		for (var i = 0; i < TAPIC.getChatters().admins.length; i++) {
-			var tempuser = TAPIC.getChatters().admins[i];
-			output += `${tempuser} <br> `;
-		}
-		output += '</p> ';
-	}
+        for (var i = 0; i < TAPIC.getChatters().admins.length; i++) {
+            var tempuser = TAPIC.getChatters().admins[i];
+            output += `${tempuser} <br> `;
+        }
+        output += '</p> ';
+    }
 
-	if ( TAPIC.getChatters().global_mods.length > 0 ) {
-		output += `<p> <b style="color: #1a7026;">GLOBAL MODS</b> &mdash;
+    if (TAPIC.getChatters().global_mods.length > 0) {
+        output += `<p> <b style="color: #1a7026;">GLOBAL MODS</b> &mdash;
 			<b>${TAPIC.getChatters().global_mods.length.toLocaleString()}</b> <br> `;
 
-		for (var i = 0; i < TAPIC.getChatters().global_mods.length; i++) {
-			var tempuser = TAPIC.getChatters().global_mods[i];
-			output += `${tempuser} <br> `;
-		}
-		output += '</p> ';
-	}
+        for (var i = 0; i < TAPIC.getChatters().global_mods.length; i++) {
+            var tempuser = TAPIC.getChatters().global_mods[i];
+            output += `${tempuser} <br> `;
+        }
+        output += '</p> ';
+    }
 
-	if ( TAPIC.getChatters().viewers.length > 0 ) {
-		output += `<p> <b style="color: #3CF;">VIEWERS</b> &mdash;
+    if (TAPIC.getChatters().viewers.length > 0) {
+        output += `<p> <b style="color: #3CF;">VIEWERS</b> &mdash;
 			<b>${TAPIC.getChatters().viewers.length.toLocaleString()}</b> <br> `;
 
-		for (var i = 0; i < TAPIC.getChatters().viewers.length; i++) {
-			var tempuser = TAPIC.getChatters().viewers[i];
-			output += `${tempuser} <br> `;
-		}
-		output += '</p> ';
-	}
+        for (var i = 0; i < TAPIC.getChatters().viewers.length; i++) {
+            var tempuser = TAPIC.getChatters().viewers[i];
+            output += `${tempuser} <br> `;
+        }
+        output += '</p> ';
+    }
 
-	$('#userlist').html(output);
+    $('#userlist').html(output);
 }
 
 function changeStatusSubmit() {
-	var newGame = $('#updateGame').val();
-	var newStatus = $('#updateStatus').val();
-	$('#changeStatusModal').modal('hide');
+    var newStatus = $('#updateStatus').val();
+    $('#changeStatusModal').modal('hide');
 
-	TAPIC.setStatusGame( newStatus, newGame );
-	refresh();
+    TAPIC.setStatusGame(newStatus, TAPIC.getGame());
+    refresh();
+}
+
+function changeGameSubmit() {
+    var newGame = $('#updateGame').val();
+    $('#changeGameModal').modal('hide');
+
+    TAPIC.setStatusGame(TAPIC.getStatus(), newGame);
+    refresh();
 }
 
 function changeStatusOpen() {
-	$('#changeStatusModal').modal('show');
-	$('#updateGame').val( TAPIC.getGame() );
-	$('#updateStatus').val( TAPIC.getStatus() );
+    $('#changeStatusModal').modal('show');
+    $('#updateStatus').val(TAPIC.getStatus());
+}
+
+function changeGameOpen() {
+    $('#changeGameModal').modal('show');
+    $('#updateGame').val(TAPIC.getGame());
 }
